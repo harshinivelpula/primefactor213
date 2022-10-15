875d609653c0ba302a98991e35bd77ab9a76b3a3
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-15 20:08:41 +0530

    factors of prime number

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace primefactor213
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 898;
+            int i, j;
+            int count = 0;
+            int flag = 0;
+
+            Console.WriteLine("Prime factor of {0}:", num);
+            for (i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count = 0;
+                    
+                    for (j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            count++;
+                    }
+                    if (count == 2)
+                    {
+                        flag = 1;
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+
+            if (flag == 0)
+                Console.WriteLine("There is no Prime factor for {0} ", num);
+        }
+
+    }
+}
+
